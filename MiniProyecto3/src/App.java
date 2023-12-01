737c949a9064ba56dd08466910ba9856b2e661de
wilsonmartinez1007 <117456018+wilsonmartinez1007@users.noreply.github.com>
2023-11-30 23:09:53 -0500

    SeActualizomain

--- MiniProyecto3/src/App.java
+++ MiniProyecto3/src/App.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        List<Candidato> miLista = new ArrayList<>();
+
+        Candidato2 candidato2 = new Candidato2(miLista);
+        Vista vista = new Vista();
+        ControladorCiudadano controladorCiudadano = new ControladorCiudadano(candidato2, vista, scanner);
+        controladorCiudadano.iniciar();
     }
 }
